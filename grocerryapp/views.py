06a674d5f59bc56f85aeb5e29ec5318967d4c89d
Sangam-dev <parajulis295@gmail.com>
2026-02-28 18:21:40 +0545

    toggling added

--- grocerryapp/views.py
+++ grocerryapp/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import GroceryItem
 
 
 def index(request):
-    """Display all grocery items"""
     items = GroceryItem.objects.all()
     context = {
         'items': items,
     }
-    return render(request, 'index.html', context)
+    return render(request, 'grocery/index.html', context)
+
+
+def toggle_completed(request, item_id):
+
+    if request.method == 'POST':
+        item = get_object_or_404(GroceryItem, id=item_id)
+        item.completed = not item.completed
+        item.save()
+
+    return redirect('grocery:index')
