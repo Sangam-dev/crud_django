e61567b7a5168fddc314a8ba32707d6208c33a45
Sangam-dev <parajulis295@gmail.com>
2026-02-28 22:17:38 +0545

    fixed broken links and added edit and add item functionalities

--- grocerryapp/views.py
+++ grocerryapp/views.py
@@ -18,3 +18,29 @@ def toggle_completed(request, item_id):
         item.save()
 
     return redirect('grocerryapp:index')
+
+# ....
+
+def delete_item(request, item_id):
+    if request.method == 'POST':
+        item = get_object_or_404(GroceryItem, id=item_id)
+        item.delete()
+
+    return redirect('grocerryapp:index')
+
+def add_item(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        if name:
+            GroceryItem.objects.create(name=name)
+    return redirect('grocerryapp:index')
+
+def edit_item(request, item_id):
+    item = get_object_or_404(GroceryItem, id=item_id)
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        if name:
+            item.name = name
+            item.save()
+            return redirect('grocerryapp:index')
+    return render(request, 'edit.html', {'item': item})
